b58a7df545b636878cd857ab57d57c83dfc0b360
gabriel-roque <gabrielrms.dll@gmail.com>
2020-04-03 12:46:37 -0300

    feat(webapp): store and delete in resource 'itens instituicao'

--- webapp/src/app/modules/item/store/actions.js
+++ webapp/src/app/modules/item/store/actions.js
@@ -10,3 +10,27 @@ export const getItensInstituicao = ({ commit }, instituicaoID) => {
       commit(types.GET_ITENS_INSTITUICAO, resp.data.data);
     });
 };
+
+export const storeItem = ({ commit, dispatch }, item) => {
+  requisicaoAutorizada
+    .post(`${URL}/item`, item)
+    .then(() => {
+      commit(types.STORE_ITEM, item);
+      dispatch('alert/success', 'Item salvo com sucesso!', { root: true });
+    })
+    .catch(() => {
+      dispatch('alert/error', 'Falha ao salvar item.', { root: true });
+    });
+};
+
+export const deleteItem = ({ commit, dispatch }, item) => {
+  requisicaoAutorizada
+    .delete(`${URL}/item/${item.id}`)
+    .then(() => {
+      commit(types.DELETE_ITEM, item);
+      dispatch('alert/success', 'Item deletado com sucesso!', { root: true });
+    })
+    .catch(() => {
+      dispatch('alert/error', 'Falha ao deletar item.', { root: true });
+    });
+};
--- webapp/src/app/modules/item/store/mutations.js
+++ webapp/src/app/modules/item/store/mutations.js
@@ -3,5 +3,12 @@ import * as types from './types';
 export const mutations = {
   [types.GET_ITENS_INSTITUICAO](state, payload) {
     state.itens = payload;
+  },
+  [types.STORE_ITEM](state, item) {
+    state.itens.push(item);
+  },
+  [types.DELETE_ITEM](state, item) {
+    const index = state.itens.indexOf(item);
+    state.itens.splice(index, 1);
   }
 };
